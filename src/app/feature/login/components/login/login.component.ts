1af4ad8e0e8639b45909ac104fbdc454a91fca6e
Carolina <caroliina20003@gmail.com>
2022-06-08 23:35:01 -0500

    Se añade warehouse

--- src/app/feature/login/components/login/login.component.ts
+++ src/app/feature/login/components/login/login.component.ts
@@ -13,6 +13,7 @@ export class LoginComponent {
   loginForm: FormGroup;
 
   constructor(private service: LoginService, private formBuilder: FormBuilder) {
+    localStorage.setItem('token', '');
     this.loginForm = this.formBuilder.group({
       user: [undefined, [Validators.required]],
       password: [undefined, [Validators.required]]
---
+++ src/app/feature/warehouse/warehouse.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateWarehouseComponent } from './components/create-warehouse/create-warehouse.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateWarehouseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WarehouseModule { }
